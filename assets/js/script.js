b14db48f46fcc51c8ace5d378ebb4dd88a0271fa
Alex Lvq <alexandrelvqq@gmail.com>
2022-01-24 18:48:23 +0100

    Shifumi

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var entrer = document.getElementById('paper');
+
+entrer.addEventListener('click', function () {
+    document.getElementById('result').outerHTML += `You Win !`;
+})
+
+paper.addEventListener('click', function () {
+    document.getElementById('content').style.display = 'none';
+})
