7d0eef018a897ead42929e9a3ad8df91b607630a
Alex Lvq <alexandrelvqq@gmail.com>
2022-01-25 13:42:11 +0100

    Shifumi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,38 @@
-// var entrer = document.getElementById('paper');
 
-// entrer.addEventListener('click', function () {
-//     document.getElementById('result').outerHTML += `You Win !`;
-// })
+// ------------------- Drag & Drop ------------------------
 
-// paper.addEventListener('click', function () {
-//     document.getElementById('content').style.display = 'none';
-// })
-
-
-
-
-
-
-
-
-// -------------------Drag & Drop ------------------------
 var target = document.getElementById('lerond');
 var sign = document.getElementById('paper');
 var sign2 = document.getElementById('rock');
 var sign3 = document.getElementById('scissors');
 console.log('target')
 
-target.addEventListener ('dragover', function(ev) {
-    console.log('inDragOver')
+target.addEventListener('dragover', function (ev) {
     ev.preventDefault();
-  })
+})
 
-
-  //   ------------------------Paper----------------------
-sign.addEventListener('dragstart', function(ev) {
-    console.log('inDragStart')
+//   ------------------------Rock----------------------
+sign2.addEventListener('dragstart', function (ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-  })
+})
 
-    //   ------------------------Rock----------------------
-sign2.addEventListener('dragstart', function(ev) {
-    console.log('inDragStart')
+//   ------------------------Paper----------------------
+sign.addEventListener('dragstart', function (ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-  })
+})
 
-    //   ------------------------Scissors----------------------
-sign3.addEventListener('dragstart', function(ev) {
-    console.log('inDragStart')
+//   ------------------------Scissors----------------------
+sign3.addEventListener('dragstart', function (ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-  })
-  
-  target.addEventListener('drop', function(ev) {
-      console.log('inDrop')
+})
+
+target.addEventListener('drop', function (ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-  })
+})
+
+
+
+
+// ------------------- Lancement Game ------------------------
