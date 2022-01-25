a5ea4d3ef9ebd0389e21a02b5f2048362faf16eb
Alex Lvq <alexandrelvqq@gmail.com>
2022-01-25 16:40:57 +0100

    Shifumi

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@
 // ------------------- Drag & Drop ------------------------
 
 var target = document.getElementById('lerond');
+var target2 = document.getElementById('lerond2');
 var sign = document.getElementById('paper');
 var sign2 = document.getElementById('rock');
 var sign3 = document.getElementById('scissors');
-console.log('target')
+var signsCpu = ['rock', 'paper', 'scissors']
+
 
 target.addEventListener('dragover', function (ev) {
     ev.preventDefault();
@@ -36,3 +38,61 @@ target.addEventListener('drop', function (ev) {
 
 
 // ------------------- Lancement Game ------------------------
+target.addEventListener('drop', function (e) {
+    e.preventDefault();
+    winner(signsCpu[random(0, 2)], target.firstChild.id);
+})
+
+
+// -------------------------- Game ---------------------------
+function random(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+function winner(cpu, user) {
+
+    console.log('cpu', cpu)
+    console.log('user', user)
+}
+
+
+var win;
+var loose;
+
+function win() {
+    if ((you == "rock") && (computerChoice == "scissors")(you == "paper") && (computerChoice == "rock")(you == "scissors") && (computerChoice == "paper"));
+    alert('YOU WIN');
+}
+
+function loose() {
+    if ((computerChoice == "rock") && (you == "scissors")(computerChoice == "paper") && (you == "rock")(computerChoice == "scissors") && (you == "paper"));
+    alert('YOU LOOSE');
+}
+
+    // if (cpu == "rock") {
+    //     if (user == "paper") {
+    //         userScore++;
+    //         changeColor('lerond');
+    //     } else if (user == "scissors") {
+    //         computerScore++;
+    //         changeColor('computer')
+    //     }
+    // } else if (cpu == "paper") {
+    //     if (user == "rock") {
+    //         computerScore++;
+    //         changeColor('computer')
+    //     } else if (user == "scissors") {
+    //         userScore++;
+    //         changeColor('lerond')
+    //     }
+    // } else if (cpu == "scissors") {
+    //     if (user == "rock") {
+    //         userScore++;
+    //         changeColor('lerond')
+    //     } else if (user == "paper") {
+    //         computerScore++;
+    //         changeColor('computer')
+    //     }
+    // }
+
+console.log()
