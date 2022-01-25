cfb0884227c81c18dcfbfe47030d1e1e96f9ff7b
Alex Lvq <alexandrelvqq@gmail.com>
2022-01-25 12:26:59 +0100

    Shifumi

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,29 @@
 
 
 
+
+
+
+
+
 // -------------------Drag & Drop ------------------------
+var target = document.getElementById('lerond');
+var sign = document.getElementById('paper');
+console.log('target')
+
+target.addEventListener ('dragover', function(ev) {
+    console.log('inDragOver')
+    ev.preventDefault();
+  })
+  
+sign.addEventListener('dragstart', function(ev) {
+    console.log('inDragStart')
+    ev.dataTransfer.setData("text", ev.target.id);
+  })
+  
+  target.addEventListener('drop', function(ev) {
+      console.log('inDrop')
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  })
