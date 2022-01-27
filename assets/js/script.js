eb7a079327f971f16e335c303062612095413972
Alex Lvq <alexandrelvqq@gmail.com>
2022-01-27 11:09:18 +0100

    Shifumi

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,11 @@ var signsCpu = ['rock', 'paper', 'scissors'];
 var cpu = ['rock', 'paper', 'scissors'];
 var cpusign = document.querySelectorAll('.sign-')
 var computerChoice;
+var userChoice;
 var putUserScore = document.querySelector('.user-score');
 var putCpuScore = document.querySelector('.cpu-score');
 
 var playAgain = document.getElementById('playagain');
-
-var imgDrag = document.getElementById('lerond2');
 
 var result = document.getElementById('result');
 
@@ -22,44 +21,46 @@ var userScore = 0;
 var cpuScore = 0;
 
 
+var logo = document.getElementById('logo');
+// easter egg music
+logo.addEventListener('click', function () {
+    JouerSon();
+});
+
+function JouerSon() {
+    var sound = document.getElementById("music");
+    if (sound.paused) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+}
+// easter egg music
+
+
+
 //   --------------------------------------------------------
 //   ------------------------MatchMedia----------------------
 //   --------------------------------------------------------
-// var phone = window.matchMedia("(max-width: 768px)");
+var blou = document.getElementById('lerond')
+var lesign = document.querySelectorAll('.sign');
 
-// if (window.matchMedia("(max-width: 768px)").matches) {
-//     sign.addEventListener('click', function (v) {
-
-//     })
-//     sign2.addEventListener('click', function () {
-
-//     })
-//     sign3.addEventListener('click', function () {
-
-//     })
-// }
-
-var phone = window.matchMedia("(max-width:768px)");
-
-window.addEventListener('resize', function () {
-    console.log('je me resize');
-    if (phone.matches) {
-        //------------Rock-------
-        sign2.addEventListener('click', function (v) {
-            v.dataTransfer.setData("text", v.target.id);
-        })
-
-        //------------Paper---------
-        sign.addEventListener('click', function (v) {
-            v.dataTransfer.setData("text", v.target.id);
-        })
-
-        //-----------Scissors----------
-        sign3.addEventListener('click', function (v) {
-            v.dataTransfer.setData("text", v.target.id);
-        })
-    }
+lesign.forEach(function (g) {
+    g.addEventListener('click', function () {
+        if (g.id === 'paper') {
+            blou.innerHTML = '<img draggable="true" src="assets/img/paper.png" id="paper" alt="paper" class="sign sign-paper espacesign">';
+        } else if (g.id === 'rock') {
+            blou.innerHTML = '<img draggable="true" id="rock" alt="rock" class="sign sign-rock" src="assets/img/rock.png">';
+        } else if (g.id === 'scissors') {
+            blou.innerHTML = '<img draggable="true" id="scissors" alt="scissors" class="sign sign-scissors" src="assets/img/scissors.png">';
+        }
+        console.log(g)
+    });
 })
+
+
+
 
 
 
@@ -94,26 +95,26 @@ sign3.addEventListener('dragstart', function (ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 })
 
-target.addEventListener('drop', function (ev) {
-    ev.preventDefault();
-    this.style.borderColor = "#000000"
-    var data = ev.dataTransfer.getData("text");
-    ev.target.innerHTML = document.getElementById(data).outerHTML;
+// target.addEventListener('drop', function (ev) {
+//     ev.preventDefault();
+//     this.style.borderColor = "#000000"
+//     var data = ev.dataTransfer.getData("text");
+//     ev.target.innerHTML = document.getElementById(data).outerHTML;
 
 
 
-    computerChoice = signsCpu[random(0, 2)]
-    if (computerChoice === 'paper') {
-        imgDrag.innerHTML = '<img draggable="true" src="assets/img/paper.png" id="paper" alt="paper" class="sign sign-paper espacesign">';
-    } else if (computerChoice === 'rock') {
-        imgDrag.innerHTML = '<img draggable="true" id="rock" alt="rock" class="sign sign-rock" src="assets/img/rock.png">';
-    } else if (computerChoice === 'scissors') {
-        imgDrag.innerHTML = '<img draggable="true" id="scissors" alt="scissors" class="sign sign-scissors" src="assets/img/scissors.png">';
-    }
-    setTimeout(function () {
-        winner(computerChoice, target.firstChild.id);
-    }, 1000);
-})
+//     computerChoice = signsCpu[random(0, 2)]
+//     if (computerChoice === 'paper') {
+//         target2.innerHTML = '<img draggable="true" src="assets/img/paper.png" id="paper" alt="paper" class="sign sign-paper espacesign">';
+//     } else if (computerChoice === 'rock') {
+//         target2.innerHTML = '<img draggable="true" id="rock" alt="rock" class="sign sign-rock" src="assets/img/rock.png">';
+//     } else if (computerChoice === 'scissors') {
+//         target2.innerHTML = '<img draggable="true" id="scissors" alt="scissors" class="sign sign-scissors" src="assets/img/scissors.png">';
+//     }
+//     setTimeout(function () {
+//         winner(computerChoice, target.firstChild.id);
+//     }, 1000);
+// })
 function reset() {
     target.innerHTML = '';
     target2.innerHTML = '';
